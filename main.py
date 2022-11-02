6f92b9ac5651060f0d38fca22fb49b3f29c64029
Hank <hz4154261@gmail.com>
2022-11-02 14:10:11 +0800

    fix

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Make a random dataset:
+height = [3, 12, 5, 18, 45]
+bars = ['A', 'B', 'C', 'D', 'E']
+height.append(100)
+bars.append('Hank')
+y_pos = np.arange(len(bars))
+
+# Create bars
+plt.bar(y_pos, height)
+# Create names on the x-axis
+plt.xticks(y_pos, bars)
+
+# Show graphic
+plt.show()
